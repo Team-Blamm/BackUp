578750d1fbb962124791b5cdcccfeee816b4ca1f
Matt McDonald <mattmcd1221@gmail.com>
2017-08-01 10:24:53 -0400

    mock index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './App';
+import SuperHeroes from './';
+
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+
+
+        <Route path="/superheros" component={SuperHeroes} />
+        <Route path="/" component={App} />
+
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
