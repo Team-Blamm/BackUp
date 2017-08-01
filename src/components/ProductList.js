357ba43150b9e99f07b9daecdf67edea3ca205a5
Matt McDonald <mattmcd1221@gmail.com>
2017-08-01 14:36:27 -0400

    set up initial fetch for product page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,1 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import ProductCard from "./ProductCard.js";
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: []
+    };
+  }
+  componentDidMount() {
+    console.log("'Product List' did mount");
+    fetch("https://blamm-store-backend.herokuapp.com/api/v1/products/type/villain")
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ productList: data });
+        console.log(this.state.productList);
+      });
+  }
+
+  render() {
+    let match = this.props.match;
+    return (
+      <div>
+      {this.state.productList.count > 0 ? (
+        <div>
+          <div>Product Cards</div>
+          <ProductCard productList={this.state.productList} />
+        </div>
+      ) : (
+        <div>Loading</div>
+      )
+    }
+
+    </div>);
+  }
+}
+
+export default ProductList;
